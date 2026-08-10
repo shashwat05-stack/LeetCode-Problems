a8a188347fe412ab49429791e7f833e15273cb40
Shashwat Gupta <shashwatgupta205@gmail.com>
2026-08-10 07:10:42 +0530

    Time: 9 ms (43.09%), Space: 41.8 MB (86.1%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public int fib(int n) {
-        if(n<2){
+        if(n < 2){
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        return fib(n - 1) + fib(n - 2);
     }
 }
